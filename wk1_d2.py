40254b271117f70964e3b566b22e6516a01821de
sweta ghimire <142021966+swet121@users.noreply.github.com>
2026-07-01 15:22:26 +0545

    feat: day-2 variables and BMI calculator

---
+++ wk1_d2.py
@@ -0,0 +1,20 @@
+age = 12  # int
+height = 1.75  # float
+name = "Sweta"  # str
+is_student = 0  # bool
+middle_name = None  # NoneType
+
+print(type(age))  # type checking
+
+print(int(height))  # type casting float to int
+
+# Floor division gives quotient of the division and  rounds down to the next lowest whole integer.
+print(10 // 3)  # Output: 3
+
+""" Here age, name, is_student, middle_name are variables of different data types. The type() function is used to check the data type of the variable.
+Variable naming rules:
+1. Variable names can contain letters, numbers, and underscores.
+2. Variable names must start with a letter or an underscore.
+3. Variable names are case-sensitive.
+4. Variable names cannot be a reserved keyword in Python.
+"""
